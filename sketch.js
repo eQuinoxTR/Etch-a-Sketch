b9d562f35b5edb17475cb54339fa63b9775ecaa2
eQuinoxTR <123718259+eQuinoxTR@users.noreply.github.com>
2025-01-25 01:32:31 -0300

    added color!

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let amountOfSquares = 100; 
 const container = document.querySelector(".container");
 const input = document.querySelector("#userInput");
+let color = document.querySelector(".colorPicker")
 createGrid();
 
 input.addEventListener("input", () => {
@@ -13,23 +14,27 @@ input.addEventListener("input", () => {
 function createGrid (){
     if (amountOfSquares > 100) {amountOfSquares = 100;}
 
-for (let column = 0; column < amountOfSquares; column++){
-    let gridColumnContainers = document.createElement("div");
-    gridColumnContainers.classList.add("gridColumnContainers")
-    container.appendChild(gridColumnContainers)
+    for (let column = 0; column < amountOfSquares; column++){
+        let gridColumnContainers = document.createElement("div");
+        gridColumnContainers.classList.add("gridColumnContainers")
+        container.appendChild(gridColumnContainers)
 
-    for (let row = 0; row < amountOfSquares; row++) {
-        let gridRowDivs = document.createElement("div");
-        gridRowDivs.classList.add("grid");
-        gridColumnContainers.appendChild(gridRowDivs);
-    }
+        for (let row = 0; row < amountOfSquares; row++) {
+            let gridRowDivs = document.createElement("div");
+            gridRowDivs.classList.add("grid");
+            gridColumnContainers.appendChild(gridRowDivs);
+        }
 }
     let grid = document.querySelectorAll(".grid");
         grid.forEach( grid => {
             grid.addEventListener("mousemove", (e) => {
-                if (e.buttons) { grid.setAttribute("id", "clickAfter"); }
+                if (e.buttons) {
+                     grid.setAttribute("id", "clickAfter"); 
+                     grid.style.background = `${color.value}`
+                    }
         })
     })
+
 }
     const div = document.querySelector("div")
     div.addEventListener("dragstart", (e) => {e.preventDefault()});
