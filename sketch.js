383520c7d5a51cac9e7acc1dc53ae4d4f6461f3e
eQuinoxTR <123718259+eQuinoxTR@users.noreply.github.com>
2025-01-26 20:22:18 -0300

    added movement for no reason :)

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
-let amountOfSquares = 100; 
+let amountOfSquares = 50; 
 let root = document.documentElement;
 const container = document.querySelector(".container");
 const input = document.querySelector("#userInput");
 let color = document.querySelector(".colorPicker")
+const centerX = root.clientWidth/2;
+const centerY = root.clientHeight/2
 createGrid();
 
 input.addEventListener("input", () => {
@@ -13,7 +15,7 @@ input.addEventListener("input", () => {
 });
 
 function createGrid (){
-    if (amountOfSquares > 100) {amountOfSquares = 100;}
+    if (amountOfSquares > 70) {amountOfSquares = 70;}
 
     for (let column = 0; column < amountOfSquares; column++){
         let gridColumnContainers = document.createElement("div");
@@ -29,16 +31,22 @@ function createGrid (){
     let grid = document.querySelectorAll(".grid");
         grid.forEach( grid => {
             grid.addEventListener("mousemove", (e) => {
-                if (e.buttons) {
+                if (e.buttons == "1") {
                      grid.setAttribute("id", "clickAfter"); 
                      grid.style.background = `${color.value}`
                     }
         })
     })
-
 }
+    root.addEventListener("mousemove", (e) => {
+        if (e.buttons == "2") {
+            root.style.setProperty('--mouse-x', (e.clientX - centerX) + "px"); // to the center elements if right click, im experimenting for the sake of practice
+            root.style.setProperty('--mouse-y', (e.clientY - centerY) + "px");
+        }
+    }); // 958 450
     const div = document.querySelector("div")
     div.addEventListener("dragstart", (e) => {e.preventDefault()});
+    root.addEventListener("contextmenu", (e) => {e.preventDefault()});
 
     function toggleTheme(){
         let changeTheme = root.classList.value == "light-theme" ? "dark-theme" : "light-theme";
